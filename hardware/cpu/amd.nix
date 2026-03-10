29404777ba08eb5080195a8e37bfc3b907dfad09
AveryanAlex <alex@averyan.ru>
2026-03-11 00:28:48 +0300

    feat(hardware): enable amd_pstate active mode for Zen 3 CPUs

--- hardware/cpu/amd.nix
+++ hardware/cpu/amd.nix
@@ -1,9 +1,10 @@
 { config, ... }:
 {
   boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelParams = [ "amd_pstate=active" ];
   hardware.enableRedistributableFirmware = true;
   hardware.cpu.amd.updateMicrocode = true;
-  powerManagement.cpuFreqGovernor = "schedutil";
+  powerManagement.cpuFreqGovernor = "powersave";
 
   boot.extraModulePackages = [ config.boot.kernelPackages.zenpower ];
 }
