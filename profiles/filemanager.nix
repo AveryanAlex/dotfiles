b2afb116e7bc271dd47bfae42a2c3dc486653a18
AveryanAlex <alex@averyan.ru>
2026-03-22 18:09:14 +0300

    filemanager: migrate GTK bookmarks from gnome.nix

--- profiles/filemanager.nix
+++ profiles/filemanager.nix
@@ -6,5 +6,13 @@
     home.packages = with pkgs; [
       nautilus
     ];
+
+    home.file.".config/gtk-3.0/bookmarks".text = ''
+      file:///home/alex/projects Projects
+      file:///tank Tank
+      file:///home/alex/Documents
+      file:///home/alex/Pictures
+      file:///home/alex/Downloads
+    '';
   };
 }
