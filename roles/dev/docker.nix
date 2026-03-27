8193f8ae397c0b380ad8914e2d0aa12f9d529a4d
AveryanAlex <alex@averyan.ru>
2026-03-27 20:04:52 +0300

    docker: reuse podman registry auth via symlink

--- roles/dev/docker.nix
+++ roles/dev/docker.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, ... }:
 {
   virtualisation.docker.rootless = {
     enable = true;
@@ -10,4 +10,11 @@
   };
   # hm.home.sessionVariables.DOCKER_HOST = "unix:///run/user/1000/podman/podman.sock";
   # hm.home.packages = [ pkgs.docker-client ];
+
+  # use podman's registry auth for docker, avoid ~/.docker/config.json
+  systemd.tmpfiles.rules = [
+    "d /home/alex/.config/docker 0755 alex users -"
+    "L+ /home/alex/.config/docker/config.json - - - - ${config.age.secrets.podman-auth.path}"
+  ];
+  hm.home.sessionVariables.DOCKER_CONFIG = "/home/alex/.config/docker";
 }
