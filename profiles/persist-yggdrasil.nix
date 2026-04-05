6b7567d58beefddee97554343809cc1042aec758
AveryanAlex <alex@averyan.ru>
2026-04-05 22:34:37 +0300

    fix: use PrivateKeyPath for yggdrasil

--- profiles/persist-yggdrasil.nix
+++ profiles/persist-yggdrasil.nix
@@ -1,31 +1,11 @@
 {
   config,
-  lib,
   secrets,
   ...
 }:
 {
-  # users = {
-  #   users.yggdrasil = {
-  #     isSystemUser = true;
-  #     description = "Yggdrasil";
-  #     group = "yggdrasil";
-  #     uid = 728;
-  #   };
-  #   groups.yggdrasil.gid = 728;
-  # };
-
-  # systemd.services.yggdrasil = {
-  #   serviceConfig = {
-  #     DynamicUser = lib.mkForce false;
-  #     User = "yggdrasil";
-  #   };
-  # };
-
   age.secrets.yggdrasil-keys = {
     file = "${secrets}/yggdrasil/${config.networking.hostName}.age";
-    # owner = "yggdrasil";
-    # group = "yggdrasil";
   };
-  # services.yggdrasil.settings.PrivateKeyPath = config.age.secrets.yggdrasil-keys.path;
+  services.yggdrasil.settings.PrivateKeyPath = config.age.secrets.yggdrasil-keys.path;
 }
