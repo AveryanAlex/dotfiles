b927d594659f8f9d8f45fa4bf04f17e1856dac3b
AveryanAlex <alex@averyan.ru>
2026-04-07 18:42:33 +0300

    feat(core): auto-start zellij web with nebula proxy

--- roles/core/shell/zellij.nix
+++ roles/core/shell/zellij.nix
@@ -1,3 +1,8 @@
+{ pkgs, ... }:
+let
+  zellijWebPort = 38082;
+  zellijWebProxyPort = 8082;
+in
 {
   hm.programs.zellij = {
     enable = true;
@@ -5,11 +10,56 @@
     settings = {
       default_mode = "locked";
       default_shell = "zsh";
+      session_serialization = false;
       serialization_interval = 60;
       scroll_buffer_size = 100000;
       pane_frames = false;
       on_force_close = "detach";
       show_startup_tips = false;
+      web_server_ip = "127.0.0.1";
+      web_server_port = zellijWebPort;
     };
   };
+
+  hm.systemd.user.services.zellij-web = {
+    Unit = {
+      Description = "Zellij web server";
+      After = [ "network-online.target" ];
+      Wants = [ "network-online.target" ];
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.zellij}/bin/zellij web";
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+
+  hm.systemd.user.services.zellij-web-http-proxy = {
+    Unit = {
+      Description = "Zellij web HTTP proxy";
+      After = [
+        "network-online.target"
+        "zellij-web.service"
+      ];
+      Wants = [
+        "network-online.target"
+        "zellij-web.service"
+      ];
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.socat}/bin/socat TCP-LISTEN:${toString zellijWebProxyPort},bind=0.0.0.0,reuseaddr,fork TCP:127.0.0.1:${toString zellijWebPort}";
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+
+  networking.firewall.interfaces."nebula.averyan".allowedTCPPorts = [ zellijWebProxyPort ];
 }
