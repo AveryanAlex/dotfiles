501e57e22d12d33fa196fd4d505f077e22673624
AveryanAlex <alex@averyan.ru>
2026-05-24 16:57:39 +0300

    Install lilipod

--- roles/desktop/distrobox.nix
+++ roles/desktop/distrobox.nix
@@ -1,13 +1,17 @@
-{
-  # enable distrobox
-  hm.programs.distrobox.enable = true;
+{pkgs, ...}: {
+  hm = {
+    # enable distrobox
+    programs.distrobox.enable = true;
 
-  # separate home by default
-  hm.home.sessionVariables.DBX_CONTAINER_HOME_PREFIX = "/home/alex/Distrobox";
+    home.packages = with pkgs; [lilipod];
 
-  # convenient aliases to enter containers
-  hm.home.shellAliases = {
-    arch = "distrobox enter archlinux";
-    deb = "distrobox enter ubuntu";
+    # separate home by default
+    home.sessionVariables.DBX_CONTAINER_HOME_PREFIX = "/home/alex/Distrobox";
+
+    # convenient aliases to enter containers
+    home.shellAliases = {
+      arch = "distrobox enter archlinux";
+      deb = "distrobox enter ubuntu";
+    };
   };
 }
