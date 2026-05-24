db8a40e957790c04eb656ee0df695513f25c676c
AveryanAlex <alex@averyan.ru>
2026-05-24 16:52:48 +0300

    Disable codex declarative config

--- roles/dev/codex.nix
+++ roles/dev/codex.nix
@@ -2,27 +2,24 @@
 {
   hm.programs.codex = {
     enable = true;
-    enableMcpIntegration = true;
-    settings = {
-      # Disables all user confirmation prompts for actions. Extremely dangerous --
-      # remove this setting if you're new to Codex.
-      # approval_policy = "never";
-
-      # Grants unrestricted system access: AI can read/write any file and execute
-      # network-enabled commands. Highly risky -- remove this setting if you're
-      # new to Codex.
-      sandbox_mode = "danger-full-access";
-
-      model = "gpt-5.4";
-      model_provider = "litellm";
-      model_reasoning_effort = "high";
-
-      model_providers.litellm = {
-        name = "LiteLLM";
-        base_url = "https://litellm.averyan.ru/v1";
-        env_key = "OPENAI_API_KEY";
-        wire_api = "responses";
-      };
-    };
+    # enableMcpIntegration = true;
+    #     settings = {
+    #       # Grants unrestricted system access: AI can read/write any file and execute
+    #       # network-enabled commands. Highly risky -- remove this setting if you're
+    #       # new to Codex.
+    #       sandbox_mode = "danger-full-access";
+    #
+    #       model = "gpt-5.5";
+    #       model_provider = "omniroute";
+    #       model_reasoning_effort = "high";
+    #       # openai_base_url = "https://omniroute.neutrino.su/api/v1";
+    #
+    #       model_providers.omniroute = {
+    #         name = "OmniRoute";
+    #         base_url = "https://omniroute.neutrino.su/api/v1";
+    #         env_key = "OMNIROUTE_API_KEY";
+    #         wire_api = "responses";
+    #       };
+    #     };
   };
 }
